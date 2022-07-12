114982e334def1333db47b6e4b26c926910bd2cb
kamalakar141181@gmail.com <kamalakar141181@gmail.com>
2022-07-12 16:12:14 +0530

    Fresh Build

--- Calculator/Operations.cs
+++ Calculator/Operations.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to custom calculator - ngrok configuration added with https.");            
+            Console.WriteLine("Welcome to custom calculator - fresh build.");            
         }
 
         public double Add(double num1, double num2)
